e6aed6bbab5f4817f5341ea5e892ee7113411f81
Joe Au-Yeung <j.auyeung419@gmail.com>
2021-03-04 21:42:19 -0500

    Add dashboard and login ejs and routes

--- app.js
+++ app.js
@@ -5,15 +5,20 @@ const connectDB = require("./config/db");
 const morgan = require("morgan");
 const ejs = require("ejs");
 
+// Connect to database
 connectDB()
 
-
+// Initiate app and EJS
 const app = express();
 app.set("view engine", "ejs");
 
+// Use morgan to log any requests
 if (process.env.NODE_ENV === "development") {
     app.use(morgan("dev"));
 }
+
+// Routes
+app.use("/", require("./routes/index"));
 
 
 const PORT = process.env.PORT || 5000;
