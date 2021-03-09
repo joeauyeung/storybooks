226a454981537ea2eaad5ba91f04ac7eec3a122c
Joe Au-Yeung <j.auyeung419@gmail.com>
2021-03-08 19:42:32 -0500

    Add navbar to header partial

--- routes/auth.js
+++ routes/auth.js
@@ -16,4 +16,12 @@ router.get("/google/callback",
         res.redirect("/dashboard");
     })
 
+// @desc    Logout user
+// @route   /auth/logout
+
+router.get("/logout", (req, res) => {
+    req.logout();
+    res.redirect("/");
+})
+
 module.exports = router;
