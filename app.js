9c712fe51e5e3c19b4657d76b0dbbced9098ccb7
Joe Au-Yeung <j.auyeung419@gmail.com>
2021-03-09 19:01:09 -0500

    Add form and CKEDITOR to add stories page

--- app.js
+++ app.js
@@ -49,6 +49,7 @@ if (process.env.NODE_ENV === "development") {
 // Routes
 app.use("/", require("./routes/index"));
 app.use("/auth", require("./routes/auth"));
+app.use("/stories", require("./routes/stories"));
 
 const PORT = process.env.PORT || 8080;
 
