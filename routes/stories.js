8490ef1460ffd0039cff1d223f9b1e17fd21c9ac
Joe Au-Yeung <j.auyeung419@gmail.com>
2021-03-10 20:49:49 -0500

    Create story edit button and story delete button and route

--- routes/stories.js
+++ routes/stories.js
@@ -49,27 +49,33 @@ router.get("/", ensureAuth, async(req, res) => {
 // @desc    Show edit story page
 // @route   GET /stories/edit/:id
 router.get("/edit/:id", ensureAuth, async (req, res) => {
-    const story = await Story.findOne({ _id: req.params.id }).lean();
+    try {
+        const story = await Story.findOne({ _id: req.params.id }).lean();
 
-    if (!story) {
-        return res.render("error/404");
+        if (!story) {
+            return res.render("error/404");
+        }
+    
+        if (String(story.user) !== req.user.id) {
+            res.redirect("/stories");
+    
+        } else {
+            res.render("stories/edit", {
+                story: story
+            });
+        }
+    } catch (error) {
+        console.error(error);
+        res.render("error/500");
     }
 
-    if (String(story.user) !== req.user.id) {
-        res.redirect("/stories");
-
-    } else {
-        res.render("stories/edit", {
-            story: story
-        });
-    }
 });
 
 // @desc    Update story
 // @route   PUT /stories/:id
-
 router.put("/:id", ensureAuth, async (req, res) => {
-    let story = await Story.findById(req.params.id).lean();
+    try {
+        let story = await Story.findById(req.params.id).lean();
 
     if (!story) {
         return res.render("error/404");
@@ -85,6 +91,23 @@ router.put("/:id", ensureAuth, async (req, res) => {
     }
 
     res.redirect("/stories");
+    } catch (error) {
+        console.log(error);
+        res.render("error/500");
+    }
+    
+})
+
+// @desc    Delete story
+// @desc    DELETE stories/:id
+router.delete("/:id", ensureAuth, async (req, res) => {
+    try {
+        await Story.remove({ _id: req.params.id});
+        res.redirect("/dashboard");
+    } catch (error) {
+        console.error(error);
+        return res.render("error/500");
+    }
 })
 
 module.exports = router;
