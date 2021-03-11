88496e5b4ab57c45ac27acb631fe6a589306798a
Joe Au-Yeung <j.auyeung419@gmail.com>
2021-03-10 20:16:17 -0500

    Create edit story page and route

--- routes/stories.js
+++ routes/stories.js
@@ -39,12 +39,30 @@ router.get("/", ensureAuth, async(req, res) => {
             userId: req.user.id,
             stories: stories
         })
-        
+
     } catch (error) {
         console.error(error);
         res.render("error/500");
     }
 })
 
+// @desc    Show edit story page
+// @route GET /stories/edit/:id
+router.get("/edit/:id", ensureAuth, async (req, res) => {
+    const story = await Story.findOne({ _id: req.params.id }).lean();
+
+    if (!story) {
+        return res.render("error/404");
+    }
+
+    if (String(story.user) !== req.user.id) {
+        res.redirect("/stories");
+
+    } else {
+        res.render("stories/edit", {
+            story: story
+        });
+    }
+});
 
 module.exports = router;
